e122785af791b016d07e9a9de12f73091d5a59b2
zchengo <1933757688@qq.com>
2023-01-04 23:19:32 +0800

    style: change src and title

--- web/src/views/Result.vue
+++ web/src/views/Result.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="margin-top: 50px;">
-        <a-result status="403" sub-title="您需要订阅个人版才能使用该功能哦！">
+        <a-result status="403" sub-title="您需要订阅专业版或高级版才能使用该功能哦！">
             <template #extra>
                 <a-button type="primary" @click="onBuy">立即订阅</a-button>
             </template>
