bb23c65370463af0b1b1ec23b7eda019c604cba9
zchengo <1933757688@qq.com>
2023-01-24 19:50:04 +0800

    initial common.go in dao module

---
+++ server/dao/common.go
@@ -0,0 +1,17 @@
+package dao
+
+const (
+
+	// 数据库表名
+	USER        = "user"
+	CUSTOMER    = "customer"
+	CONTRACT    = "contract"
+	PRODUCT     = "product"
+	SUBSCRIBE   = "subscribe"
+	NOTICE      = "notice"
+	MAIL_CONFIG = "mail_config"
+
+	// 空值
+	NumberNull = 0
+	StringNull = ""
+)
